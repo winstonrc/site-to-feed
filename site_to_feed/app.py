8b82c082a8c0421d87ce9bbffed81faa05a89413
Winston Cooke <git@winstoncooke.com>
2024-02-10 13:31:10 -0600

    implement htmx

--- site_to_feed/app.py
+++ site_to_feed/app.py
@@ -6,9 +6,11 @@ import requests
 
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
+from flask_htmx import HTMX
 
 
 app = Flask(__name__)
+htmx = HTMX(app)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -28,13 +30,12 @@ def documentation():
     return render_template('documentation.html')
 
 
-@app.route('/get_html', methods=['GET'])
+@app.route('/get_html_source', methods=['GET'])
 def step_1():
     url = request.args.get('url')
-    logger.debug(f"/get_html: {url=}")
-
     if not url:
-        return f'<p>Error: URL required</p>'
+        return f'<p>Error: URL is required.</p>'
+    logger.debug(f"/get_html: {url=}")
 
     try:
         response = requests.get(url)
@@ -44,7 +45,10 @@ def step_1():
 
         cleaned_html = nh3.clean(html=html_source)
 
-        return render_template('get_html.html', html_source=cleaned_html, url=url)
+        if htmx:
+            return render_template('step_2_extract_html_htmx.html', html_source=cleaned_html)
+        else:
+            return render_template('step_2_extract_html.html', html_source=cleaned_html, url=url)
     except requests.exceptions.ConnectionError as error:
         logger.error(f"{error=}")
         return f'<p>Error: Invalid URL</p>'
@@ -55,20 +59,6 @@ def step_1():
 
 @app.route('/extract_html', methods=['POST'])
 def step_2():
-    ####################
-    # Inherited values #
-    ####################
-    url = request.form.get('url')
-    if not url:
-        return f'<p>Error: URL from step 1 is required.</p>'
-
-    html_source = request.form.get('html-source')
-    if not html_source:
-        return f'<p>Error: HTML source from step 1 is required.</p>'
-
-    ##############
-    # New values #
-    ##############
     global_search_pattern = request.form.get('global-search-pattern')
     if not global_search_pattern:
         return f'<p>Error: A string is required for Global Search Pattern.</p>'
@@ -76,6 +66,10 @@ def step_2():
     item_search_pattern = request.form.get('item-search-pattern')
     if not item_search_pattern:
         return f'<p>Error: A string is required for Item Search Pattern.</p>'
+
+    html_source = request.form.get('html-source')
+    if not html_source:
+        return f'<p>Error: HTML source from step 1 is required.</p>'
 
     translation_table = str.maketrans("", "", '{}*%"=<>/')
 
@@ -136,33 +130,19 @@ def step_2():
     logger.debug(
         f"{global_search_pattern=}\n{item_search_pattern=}\n{extracted_html=}"
     )
-    return render_template('extract_html.html', extracted_html=extracted_html, html_source=html_source, url=url)
+
+    if htmx:
+        return render_template('step_3_define_output_format_htmx.html', extracted_html=extracted_html)
+    else:
+        url = request.form.get('url')
+        if not url:
+            return f'<p>Error: URL from step 1 is required.</p>'
+
+        return render_template('step_3_define_output_format.html', extracted_html=extracted_html, html_source=html_source, url=url)
 
 
 @app.route('/format_feed_output', methods=['POST'])
 def step_3():
-    ####################
-    # Inherited values #
-    ####################
-    url = request.form.get('url')
-    if not url:
-        return f'<p>Error: URL from step 1 is required.</p>'
-
-    html_source = request.form.get('html-source')
-    if not html_source:
-        return f'<p>Error: HTML from step 1 is required.</p>'
-
-    extracted_html = request.form.get('extracted-html')
-    if not extracted_html:
-        return f'<p>Error: Extracted HTML from step 2 is required.</p>'
-
-    # Convert extracted_html from a str back into a dict to pass onto the next
-    # template and keep the data persisted in the block above.
-    extracted_html = ast.literal_eval(extracted_html)
-
-    ##############
-    # New values #
-    ##############
     feed_title = request.form.get('feed-title')
     if not feed_title:
         return f'<p>Error: A string is required for Feed Title.</p>'
@@ -191,9 +171,16 @@ def step_3():
     if not item_content_template:
         return f'<p>Error: A string is required for Item Content Template.</p>'
 
+    extracted_html = request.form.get('extracted-html')
+    if not extracted_html:
+        return f'<p>Error: Extracted HTML from step 2 is required.</p>'
+
+    # Convert extracted_html from a str back into a dict to pass onto the next
+    # template and keep the data persisted in the block above.
+    extracted_html = ast.literal_eval(extracted_html)
+
     item_content_position = extract_number(item_content_template)
 
-    # todo: implement
     feed_preview = {}
     for key, values in extracted_html.items():
         feed_preview[key] = [
@@ -202,7 +189,20 @@ def step_3():
             values[item_content_position]
         ]
 
-    return render_template('format_feed_output.html', feed_title=feed_title, feed_link=feed_link, feed_description=feed_description, feed_preview=feed_preview, extracted_html=extracted_html, html_source=html_source, url=url)
+    # todo: implement feed
+
+    if htmx:
+        return render_template('step_4_get_rss_feed_htmx.html', feed_title=feed_title, feed_link=feed_link, feed_description=feed_description, feed_preview=feed_preview)
+    else:
+        url = request.form.get('url')
+        if not url:
+            return f'<p>Error: URL from step 1 is required.</p>'
+
+        html_source = request.form.get('html-source')
+        if not html_source:
+            return f'<p>Error: HTML from step 1 is required.</p>'
+
+        return render_template('step_4_get_rss_feed.html', feed_title=feed_title, feed_link=feed_link, feed_description=feed_description, feed_preview=feed_preview, extracted_html=extracted_html, html_source=html_source, url=url)
 
 
 def extract_number(number_str):
